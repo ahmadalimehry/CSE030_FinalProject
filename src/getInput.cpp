7a4f0e63ebb7efd0e377c92b5c6b24f5f941aa9d
John Olivas <johnolivas7553@gmail.com>
2025-12-09 05:23:48 +0000

    getStart implemented

--- src/getInput.cpp
+++ src/getInput.cpp
@@ -2,5 +2,30 @@
 #include <Graph.h>
 
 Heuristic getHeuristic() { return Heuristic::COST; }
-Vertex *getStart(Graph *g);
+
+Vertex *getStart(Graph *g) {
+    Vertex *start;
+    bool found;
+
+    std::string userStart = "";
+    std::cin >> userStart;
+
+    for(int i = 0; i < g->vertices.size(); i++){
+        if(g->vertices[i]->data == userStart){
+            found = true;
+            start = g->vertices[i];
+        }
+    }
+
+    if(found){
+        return start;
+        std::cout << "Departing airport found" << std::endl;
+    } else {
+        return nullptr;
+        std::cout << "Departing airport not found" << std::endl;
+    }
+
+    return nullptr;
+}
+
 Vertex *getStop(Graph *g);
