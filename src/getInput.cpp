dfce1252b1fb681abf43e6840b49d6ef931ccddc
Sergberg111 <S_mora1021@icloud.com>
2025-12-09 18:13:34 -0800

    Improve getHeuristic to allow user input for preference
    Enhanced getHeuristic function to prompt user for search preference and handle input.

--- src/getInput.cpp
+++ src/getInput.cpp
@@ -1,6 +1,29 @@
 #include "getInput.h"
+#include <iostream>
+using namespace std;
 
-Heuristic getHeuristic() { return Heuristic::COST; }
+Heuristic getHeuristic() {
+    cout << "\nChoose a search preference:\n";
+    cout << "1. Cheapest price\n";
+    cout << "2. Shortest travel time\n";
+    cout << "3. Least stops\n";
+    cout << "Enter choice (1-3): ";
+
+    int choice;
+    cin >> choice;
+
+    switch (choice) {
+        case 1: return COST;
+        case 2: return TIME;
+        case 3: return STOPS;
+        default:
+            cout << "Invalid input. Defaulting to least stops.\n";
+            return STOPS;
+    }
+}
+
+
+
 
 Vertex *getStart(Graph *g) {
     Vertex *start;
